ac14ef3110232514a5b9005706ed7a91eafc9887
UdithaJ <it19138114@my.sliit.lk>
2025-11-16 01:14:10 +0530

    sidebar changes

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -18,11 +18,11 @@
       elevation="2"
     >
       <v-list dense nav>
-        <v-list-item class="pt-4 pb-4 d-flex align-center">
-          <v-list-item-avatar size="40">
+        <v-list-item class="pt-6 pb-4 d-flex flex-column align-center">
+          <v-list-item-avatar size="88" class="sidebar-avatar">
             <v-img :src="userImg" />
           </v-list-item-avatar>
-          <v-list-item-content>
+          <v-list-item-content class="text-center mt-2">
             <v-list-item-title class="white--text">{{ userName || 'User' }}</v-list-item-title>
             <v-list-item-subtitle class="white--text">{{ userStore.userRole || '' }}</v-list-item-subtitle>
           </v-list-item-content>
@@ -31,31 +31,47 @@
         <v-divider class="my-2" />
 
         <v-list-item @click="navigate('/app/dashboard')" class="d-flex align-center">
-          <v-list-item-icon>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Dashboard</v-list-item-title>
+          <template v-slot:prepend>
+            <v-list-item-icon class="icon-spacer">
+              <v-icon>mdi-view-dashboard</v-icon>
+            </v-list-item-icon>
+          </template>
+          <v-list-item-content>
+            <v-list-item-title class="text-no-wrap">Dashboard</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item v-if="isAdmin" @click="navigate('/app/users')" class="d-flex align-center">
-          <v-list-item-icon>
-            <v-icon>mdi-account-multiple</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Users</v-list-item-title>
+          <template v-slot:prepend>
+            <v-list-item-icon class="icon-spacer">
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+          </template>
+          <v-list-item-content>
+            <v-list-item-title class="text-no-wrap">Users</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item @click="navigate('/app/inquiries')" class="d-flex align-center">
-          <v-list-item-icon>
-            <v-icon>mdi-file-document-outline</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Inquiries</v-list-item-title>
+          <template v-slot:prepend>
+            <v-list-item-icon class="icon-spacer">
+              <v-icon>mdi-file-document-outline</v-icon>
+            </v-list-item-icon>
+          </template>
+          <v-list-item-content>
+            <v-list-item-title class="text-no-wrap">Inquiries</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item v-if="isAdmin" @click="navigate('/app/sections')" class="d-flex align-center">
           <template v-slot:prepend>
-            <v-icon color="blue-darken-2" :icon="mdi-clock"></v-icon>
+            <v-list-item-icon class="icon-spacer">
+              <v-icon>mdi-format-list-bulleted</v-icon>
+            </v-list-item-icon>
           </template>
-          <v-list-item-title>Sections</v-list-item-title>
+          <v-list-item-content>
+            <v-list-item-title class="text-no-wrap">Sections</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -93,4 +109,9 @@ const logout = () => { userStore.logout(); router.push({ name: 'login' }) }
 .menu a.router-link-active { font-weight: bold; }
 .logout-btn { background: none; border: none; color: white; cursor: pointer; font-size: 1em; padding: 0 }
 .logout-btn:hover { text-decoration: underline }
+.icon-spacer { margin-right: 12px; min-width: 24px; }
+.v-list-item { align-items: center; }
+.text-no-wrap { white-space: nowrap; }
+.sidebar-avatar { width: 112px; height: 112px; }
+.sidebar-avatar .v-image__image { width: 112px !important; height: 112px !important; object-fit: cover; border-radius: 50%; }
 </style>
